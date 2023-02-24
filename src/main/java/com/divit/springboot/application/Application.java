9f99c1300ddc6ad0d55be332db523c7baaf110c9
GurUNathaN1611 <gurunathan1625@gmail.com>
2023-02-24 10:44:54 +0530

    Added Comment
    yjytju

--- src/main/java/com/divit/springboot/application/Application.java
+++ src/main/java/com/divit/springboot/application/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+//SpringApplication
 @SpringBootApplication
 @ComponentScan("com.divit.springboot.application")
 public class Application {
